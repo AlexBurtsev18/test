b19bbc3633fa87dbc7b6503f6e45d77323c9dc32
«Alex18» <alexburtsev18@yandex.ru>
2023-06-05 12:32:27 +0300

    pravka

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(i + ", ");
+    }
+}
+
+Console.WriteLine("Введите количество элементов вашего массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[size];
+
+for(int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент: ");
+    string elem = Convert.ToString(Console.ReadLine());
+    array[i] = elem;
+}
+
+PrintArray(array);
